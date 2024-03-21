3cb215a2ad98b477a62a55ad5f19a125a44203c0
speady1445 <sykora1445@gmail.com>
2024-03-21 09:12:52 +0100

    Add Line class to draw lines into window

--- window.py
+++ window.py
@@ -22,3 +22,29 @@ class Window:
 
     def close(self):
         self._running = False
+
+    def draw_line(self, line: "Line", fill_color: str) -> None:
+        line.draw(self._canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1: Point, point2: Point) -> None:
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas: Canvas, fill_color: str) -> None:
+        canvas.create_line(
+            self.point1.x,
+            self.point1.y,
+            self.point2.x,
+            self.point2.y,
+            fill=fill_color,
+            width=2,
+        )
+        canvas.pack()
